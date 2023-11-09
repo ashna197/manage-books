95ead6df3c39a0d21ff2aeed873af4b1ccbcc053
ashna197 <37981073+ashna197@users.noreply.github.com>
2023-11-09 18:29:22 +0530

    Update dbConnection.js

--- src/database/dbConnection.js
+++ src/database/dbConnection.js
@@ -1,1 +1,21 @@
- 
+const mongoose = require('mongoose');
+
+(function () {
+  mongoose.connect('mongodb://localhost:27017/books_details', {})
+}());
+
+// Get the default connection
+const db = mongoose.connection;
+
+// Set up event listeners for the database connection
+db.on('error', (err) => {
+  console.error(`MongoDB connection error: ${err}`);
+});
+
+db.once('open', () => {
+  console.log('Connected to MongoDB');
+});
+
+db.on('disconnected', () => {
+  console.log('MongoDB connection disconnected');
+}); 
