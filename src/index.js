f2933adb407ad00ec9a0a6df3b27efc34e7e6a7d
ashna197 <37981073+ashna197@users.noreply.github.com>
2023-11-09 18:33:29 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,20 @@
- 
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+// Middleware
+app.use(bodyParser.json());
+
+require('./database/dbConnection')
+
+// API routes
+app.use('/books', require('./routes/booksRoutes'));
+app.use((req, res) => {
+    return res.status(404).json({ message: "PAGE NOT FOUND" })
+});
+
+const port = process.env.PORT || 5080;
+
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+}); 
