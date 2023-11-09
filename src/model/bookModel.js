76364357f435fc24a677a6fb008d291bf620c4a9
ashna197 <37981073+ashna197@users.noreply.github.com>
2023-11-09 18:31:11 +0530

    Update bookModel.js

--- src/model/bookModel.js
+++ src/model/bookModel.js
@@ -1,1 +1,19 @@
- 
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const bookSchema = new Schema({
+    id: { type: String, required: true, index: true },
+    title: { type: String, required: true, unique: true }, 
+    author: {
+        type: String,
+        required: true
+    },
+    summary: {
+        type: String,
+        required: true,
+    }
+});
+
+const BooksInfo = mongoose.model('BooksInfo', bookSchema, 'BooksInfo');
+
+module.exports = BooksInfo; 
